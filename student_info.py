5e62704460fcf3b5b902464e2f328bf2502ba50c
JenniferJackson <marshall_jennifer3@students.columbusstate.edu>
2026-02-01 21:15:29 -0500

    add favoriteHobby field and print favorite hobby field

--- student_info.py
+++ student_info.py
@@ -3,6 +3,7 @@ major = "Information Technology"
 graduation_year = 2026
 email = "jennifer.jackson3@columbusstate.edu"
 hometown = "Augusta, Ga"
+favoriteHobby = "Cooking"
 
 print("==========================================")
 print("       STUDENT INFORMATION CARD")
@@ -12,4 +13,5 @@ print(f"Major: {major}")
 print(f"Graduation Year: {graduation_year}")
 print(f"Email: {email}")
 print(f"Hometown: {hometown}")
+print(f"Favorite Hobby: {favoriteHobby}")
 print("==========================================")
