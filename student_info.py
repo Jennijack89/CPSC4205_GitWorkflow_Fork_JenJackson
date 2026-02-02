63ad1048558b40d7bb5bbbf5e0fbee5bf8b5a592
JenniferJackson <marshall_jennifer3@students.columbusstate.edu>
2026-02-01 21:11:02 -0500

    add hometown field, add print command for hometown field

--- student_info.py
+++ student_info.py
@@ -2,11 +2,14 @@ name = "Jennifer Jackson"
 major = "Information Technology"
 graduation_year = 2026
 email = "jennifer.jackson3@columbusstate.edu"
+hometown = "Augusta, Ga"
 
 print("==========================================")
 print("       STUDENT INFORMATION CARD")
-print("f"Email: {email}")
+print("==========================================")
 print(f"Name: {name}")
 print(f"Major: {major}")
 print(f"Graduation Year: {graduation_year}")
+print(f"Email: {email}")
+print(f"Hometown: {hometown}")
 print("==========================================")
