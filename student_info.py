87d772b5476201d53945f37a26f9b96870f1965c
JenniferJackson <marshall_jennifer3@students.columbusstate.edu>
2026-02-01 21:04:36 -0500

    Change name to Jennifer Jackson, update year to 2026, add email address field to student profile

--- student_info.py
+++ student_info.py
@@ -1,10 +1,11 @@
-name = "John Doe"
+name = "Jennifer Jackson"
 major = "Information Technology"
-graduation_year = 2025
+graduation_year = 2026
+email = "jennifer.jackson3@columbusstate.edu"
 
 print("==========================================")
 print("       STUDENT INFORMATION CARD")
-print("==========================================")
+print("f"Email: {email}")
 print(f"Name: {name}")
 print(f"Major: {major}")
 print(f"Graduation Year: {graduation_year}")
